30e28b67dc1418089135949cf299e994f2b9e057
Jonas Jurczok <jjurczok@notebooksbilliger.de>
2017-09-28 22:29:40 +0200

    updated build, added rockspec template

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -ev
+
 luacheck .
 busted
 
 if [ ${TRAVIS_BRANCH} = "master" ] && [ ${TRAVIS_TAG} != ""] && [ ${TRAVIS_PULL_REQUEST} = "false" ] ; then
 	# we are on master, not in a pull request and we have a tag. Lets deploy
-	luarocks --version
+	luarocks --api-key=${api-key}
 fi
