c48e91c62f3a7aba9402186cb477c2c10f96ea0f
Jonas Jurczok <jjurczok@notebooksbilliger.de>
2017-09-29 15:54:55 +0200

    updated spec and build script

--- build.sh
+++ build.sh
@@ -7,5 +7,8 @@ busted
 
 if [ ${TRAVIS_BRANCH} = "master" ] && [ ${TRAVIS_TAG} != ""] && [ ${TRAVIS_PULL_REQUEST} = "false" ] ; then
 	# we are on master, not in a pull request and we have a tag. Lets deploy
-	luarocks --api-key=${api-key}
+	sed -e "s/VERSION/${TRAVIS_TAG}/g" rockspec.template > faketorio-${TRAVIS_TAG}-1.rockspec
+	luarocks upload faketorio-${TRAVIS_TAG}-1.rockspec --api-key=${api-key}
+
+	rm faketorio-${TRAVIS_TAG}-1.rockspec
 fi
