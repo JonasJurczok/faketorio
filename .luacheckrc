f93e8f5c8518f468d392a2d657fbd44d76c11cba
Jonas Jurczok <jjurczok@notebooksbilliger.de>
2018-02-25 16:50:21 +0100

    #19 fix runner and luacheck config

--- .luacheckrc
+++ .luacheckrc
@@ -1,4 +1,15 @@
 
 globals = {"faketorio", "busted", "feature", "scenario", "commands", "game"}
+read_globals = {
+    os = {
+        fields = {
+            execute = {
+                fields = {
+                    revert = {}
+                }
+            }
+        }
+    }
+}
 files['spec/*_spec.lua'].std = 'max+busted'
 files['spec/*_IGNORED_.lua'].std = 'max+busted'
--- src/faketorio/runner.lua
+++ src/faketorio/runner.lua
@@ -4,8 +4,10 @@ function faketorio.parse_arguments()
     local argparse = require("argparse")
     local parser = argparse("Faketorio", "Automated testing for your Factorio mod.")
     parser:command("build",
-            "Create a folder named after the mod under \"target\" and copy all source files there. No further actions are initialized",
-            "This command can be used if you just want to check the assembled mod without actually doing anything with it. \n"..
+            "Create a folder named after the mod under \"target\" and copy all source files there. " ..
+                    "No further actions are initialized",
+            "This command can be used if you just want to check the assembled mod without actually " ..
+                    "doing anything with it. \n" ..
                     "It is also the basis for all other steps and will be called implicitly.")
     parser:command("clean",
             "Clean the working directory \"target\". This will be implicitly done by the other commands")
@@ -14,7 +16,7 @@ function faketorio.parse_arguments()
             "This is intended for when you already have factorio running and just want to quickly " ..
                     "iterate without changing translations etc.")
     parser:command("package",
-            "Build the mod for uploading to the Factorio mod portal."
+            "Build the mod for uploading to the Factorio mod portal.",
             "This command creates a zip file according to the Factorio standard " ..
                     "end and places it in the current directory.")
     parser:command("run",
