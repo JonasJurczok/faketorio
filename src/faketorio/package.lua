a1e5f72a208b0640561ef18079b1e8b6b0292371
Jonas Jurczok <jonasjurczok@gmail.com>
2018-08-19 23:16:52 +0200

    move to hopefully correct location

--- src/faketorio/package.lua
+++ src/faketorio/package.lua
@@ -3,10 +3,6 @@ if not faketorio then faketorio = {} end
 require("faketorio.helper")
 
 function faketorio.package()
-    -- apparently sometimes this is not done properly.
-    require("luarocks.core.cfg"):init()
-    require("luarocks.fs"):init()
-
     local zip = require("luarocks.tools.zip")
 
     assert(faketorio.lfs.chdir("target"))
