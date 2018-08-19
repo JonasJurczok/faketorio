5747dca1af37ee66cf6d5a8aba35ef7f93e03767
Jonas Jurczok <jonasjurczok@gmail.com>
2018-08-19 23:10:00 +0200

    hopefuly fixed zipping issues on mac and linux

--- src/faketorio/package.lua
+++ src/faketorio/package.lua
@@ -3,6 +3,10 @@ if not faketorio then faketorio = {} end
 require("faketorio.helper")
 
 function faketorio.package()
+    -- apparently sometimes this is not done properly.
+    require("luarocks.core.cfg"):init()
+    require("luarocks.fs"):init()
+
     local zip = require("luarocks.tools.zip")
 
     assert(faketorio.lfs.chdir("target"))
